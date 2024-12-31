1a3740dc9ecacfcad09c70e011907b64790154c2
Pachi-Dev <pachi.claros@gmail.com>
2024-12-31 01:25:41 -0600

    feat: Reemplazar el componente ListUsers por una tabla en la página de usuarios con opciones de edición

--- src/app/dashboard/usuarios/page.tsx
+++ src/app/dashboard/usuarios/page.tsx
@@ -1,6 +1,7 @@
 import { fetchUsers } from 'app/lib/db'
 import { AddUser } from '../../../components/users/AddUser'
-import { ListUsers } from '../../../components/users/ListUsers'
+import { EditUser } from '../../../components/users/EditUser'
+import { EditPassword } from '../../../components/users/EditPassword'
 import { User } from 'app/lib/definitions'
 import { unstable_noStore as noStore } from 'next/cache';
 
@@ -14,7 +15,33 @@ export default async function Usuarios(){
                     <h1 className="text-center font-extrabold text-2xl">Usuarios</h1>                    
                     <AddUser />
                 </div>
-                <ListUsers users={users} />
+                <div className='w-full overflow-x-auto overflow-y-hidden'>
+                    <table className='m-0 w-max min-w-full border-separate border-spacing-0 border-none p-0 text-left md:w-full'>
+                        <thead className='bg-slate-900 rounded-md'>
+                            <tr className='text-left text-xs *:font-extrabold tracking-wider text-white'>
+                                <th className="py-2 px-4 ">Nombre</th>
+                                <th className="py-2 px-4">Correo</th>
+                                <th className="py-2 px-4">Rol</th>
+                                <th className="py-2 px-4">Created</th>
+                                <th className="py-2 px-4"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {users.map((user) => (
+                                <tr key={user.id}>
+                                    <td className="py-2 px-4 border-b border-gray-200 text-sm">{user.name}</td>
+                                    <td className="py-2 px-4 border-b border-gray-200 text-sm">{user.email}</td>
+                                    <td className="py-2 px-4 border-b border-gray-200 text-sm">{user.role}</td>
+                                    <td className="py-2 px-4 border-b border-gray-200 text-sm">{user.created_at.toLocaleDateString()}</td>
+                                    <td className="py-2 px-4 border-b border-gray-200 text-sm flex gap-2 w-fit">
+                                        <EditUser user={user} />
+                                        <EditPassword user={user} />
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             </section>     
         </>
     )
