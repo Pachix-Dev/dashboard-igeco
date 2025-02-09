98223ff98e616cd5ac8b9f2c87a94a9fd3199f4f
Pachi-Dev <pachi.claros@gmail.com>
2025-02-09 17:30:10 -0600

    feat: Eliminar el flujo de trabajo de CI para Node.js y agregar .htaccess a .gitignore

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -9,7 +9,7 @@ export default function Dashboard(){
         <section className="container mx-auto px-4 my-10">            
             <h1 className="text-center font-extrabold text-4xl">Welcome {userSession?.name}!</h1>
             <div className="mt-20 flex items-center gap-2">Registra a tus expositores en la sección 
-                <Link href={'/exhibitors'} className="text-blue-600 font-bold flex items-center gap-2"> 
+                <Link href={'/dashboard/exhibitors'} className="text-blue-600 font-bold flex items-center gap-2"> 
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244" />
                     </svg>
